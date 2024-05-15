36063bbea587dd0453544955e6fa9e2b8823cee3
Jonas Rudloff <jonas.t.rudloff@gmail.com>
2024-05-15 12:27:31 +0200

    split up parse_parameter

--- src/utils.rs
+++ src/utils.rs
@@ -15,25 +15,30 @@ pub fn parse_number(number: &str) -> Result<u64> {
     }
 }
 
-pub fn parse_parameter(s: &str) -> Result<(String, Vec<u64>)> {
-    let (key, val) = s.split_once('=').context("no '=' is argument")?;
-    let mut values = vec![];
-    for value in val.split(',') {
-        values.extend(match value {
-            "rand8" => vec![rand::random::<u8>() as u64],
-            "rand16" => vec![rand::random::<u16>() as u64],
-            "rand32" => vec![rand::random::<u32>() as u64],
-            "rand64" => vec![rand::random::<u64>() as u64],
+pub fn parse_ranges(s: &str) -> Result<Vec<u64>> {
+    s.split(',').map(|value|
+        match value {
+            "rand8" => Ok(vec![rand::random::<u8>() as u64]),
+            "rand16" => Ok(vec![rand::random::<u16>() as u64]),
+            "rand32" => Ok(vec![rand::random::<u32>() as u64]),
+            "rand64" => Ok(vec![rand::random::<u64>() as u64]),
             number_or_range => {
                 if let Some((low, high)) = number_or_range.split_once('-') {
-                    (parse_number(low)?..=parse_number(high)?).collect()
+                    let low = parse_number(low).context(format!("Invalid number: {}", low))?;
+                    let high = parse_number(high).context(format!("Invalid number: {}", high))?;
+                    Ok((low..=high).collect())
                 } else {
-                    vec![parse_number(number_or_range)?]
+                    let number = parse_number(number_or_range).context(format!("Invalid number: {}", number_or_range))?;
+                    Ok(vec![number])
                 }
             }
-        })
-    }
-    Ok((key.to_string(), values))
+        }
+    ).collect::<Result<Vec<_>>>().map(|values| values.concat())
+}
+
+pub fn parse_parameter(s: &str) -> Result<(String, Vec<u64>)> {
+    let (key, ranges) = s.split_once('=').context("no '=' is argument")?;
+    Ok((key.to_string(), parse_ranges(ranges).context(format!("Invalid ranges: {}", ranges))?))
 }
 
 pub fn cartesian_product<K: Clone, V: Clone>(sets: Vec<(K, Vec<V>)>) -> Vec<Vec<(K, V)>> {
